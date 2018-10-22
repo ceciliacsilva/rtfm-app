14f54d2d80a1b179bd5563813f31fe6d67a34e54
Cecília Carneiro <cecilia.carneiroesilva@gmail.com>
2018-10-22 12:33:33 -0300

    without compilation error

---
+++ examples/blink.rs
@@ -0,0 +1,87 @@
+#![feature(panic_implementation)]
+#![no_std]
+#![no_main]
+
+extern crate cortex_m;
+extern crate cortex_m_rt;
+extern crate cortex_m_rtfm as rtfm;
+// extern crate stm32l0;
+extern crate stm32l052;
+
+use core::panic::PanicInfo;
+use core::sync::atomic::{self, Ordering};
+
+use cortex_m::asm::bkpt;
+use cortex_m::peripheral::syst::SystClkSource;
+
+use rtfm::{app, Threshold};
+
+// use stm32l0::stm32l0x1;
+// use stm32l0x1::GPIOA;
+
+use stm32l052::GPIOA;
+
+app! {
+    // device: stm32l0x1,
+    device: stm32l052,
+    resources: {
+        static ON: bool = false;
+    },
+    tasks: {
+        SYS_TICK: {
+            path: sys_tick,
+            resources: [ON],
+        },
+    }
+}
+
+fn init(mut p: init::Peripherals, r: init::Resources) {
+    // `init` can modify all the `resources` declared in `app!`
+    r.ON;
+
+    p.device.RCC.iopenr.modify(|_, w| w.iopaen().set_bit());
+
+    p.device.GPIOA.moder.modify(|_, w| unsafe{ w.mode5().bits(1) });
+
+    // gpioa.odr.modify(|_, w| w.od5().set_bit());
+
+    p.core.SYST.set_clock_source(SystClkSource::Core);
+    p.core.SYST.set_reload(2_000_000); // 1s
+    p.core.SYST.clear_current();
+    p.core.SYST.enable_counter();
+    p.core.SYST.enable_interrupt();
+}
+
+fn idle() -> ! {
+    loop {
+        rtfm::wfi();
+    }
+}
+
+#[allow(unsafe_code)]
+fn sys_tick(_t: &mut Threshold, mut r: SYS_TICK::Resources) {
+    // toggle state
+    *r.ON = !*r.ON;
+
+    if *r.ON {
+        unsafe {
+            (*GPIOA::ptr()).bsrr.write(|w| w.bs5().set_bit());
+        }
+    } else {
+        unsafe {
+            (*GPIOA::ptr()).bsrr.write(|w| w.br5().set_bit());
+        }
+    }
+}
+
+
+
+#[allow(deprecated)]
+#[panic_implementation]
+fn panic(_info: &PanicInfo) -> ! {
+    bkpt();
+
+    loop {
+        atomic::compiler_fence(Ordering::SeqCst)
+    }
+}
