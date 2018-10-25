f9c0339e7dc89cec91b0112bcc374511c393f8a0
Cecília Carneiro <cecilia.carneiroesilva@gmail.com>
2018-10-25 20:09:48 -0300

    serial example - receive_callback

--- examples/serial.rs
+++ examples/serial.rs
@@ -5,86 +5,67 @@
 extern crate cortex_m;
 extern crate cortex_m_rt;
 extern crate cortex_m_rtfm as rtfm;
-// extern crate stm32l052;
 extern crate hal;
 extern crate embedded_hal;
 extern crate cortex_m_semihosting as sh;
-
 extern crate heapless;
+#[macro_use(block)]
+extern crate nb;
 
-use heapless::Vec; // fixed capacity `std::Vec`
-use heapless::consts::U8; // type level integer used to specify capacity
-
+use heapless::Vec; 
 use heapless::spsc;
+use heapless::consts::U20; 
 
 use core::panic::PanicInfo;
 use core::sync::atomic::{self, Ordering};
 
 use cortex_m::asm::bkpt;
-// use cortex_m::peripheral::syst::SystClkSource;
 
 use rtfm::{app, Threshold, Resource};
+
+use embedded_hal::prelude::*;
 
 use hal::rcc::RccExt;
 use hal::gpio::GpioExt;
 use hal::flash::FlashExt;
 use hal::serial::Serial;
+use hal::serial::{Rx, Event, Tx};
 use hal::time::U32Ext;
-use hal::dma::DmaExt;
-// use hal::dma::{CircBuffer, dma1, Event};
-use hal::serial::{Rx, Event};
-
-use embedded_hal::prelude::*;
-
 use hal::stm32l0;
 use hal::stm32l0::stm32l0x1::USART2 as USART2_p;
-// use stm32l052::GPIOA;
 
 use hal::gpio::{Output, PushPull, gpioa::PA5};
 
-use core::fmt::Write;
-use sh::hio;
+// use core::fmt::Write;
+// use sh::hio;
+
+enum Command {
+    Enable,
+    Disable,
+    Nothing,
+}
 
 app! {
     device: stm32l0::stm32l0x1,
     resources: {
-        // static ON: bool = false;
-        // static BUFFER: [[u8; 8]; 2] = [[0; 8]; 2];
-        // static CB: CircBuffer<[u8; 8], dma1::C4>;
         static END: bool = false;
-        static VEC_DATA: Vec<u8, U8> = Vec::new();
-        static RB: spsc::Queue<u8, U8> = spsc::Queue::new();
+        static RB: spsc::Queue<u8, U20> = spsc::Queue::new();
         static RX: Rx<USART2_p>;
+        static TX: Tx<USART2_p>;
         static LED: PA5<Output<PushPull>>;
     },
     idle: {
-        resources: [LED, VEC_DATA, END, RB],
+        resources: [LED, END, RB, TX],
     },
     tasks: {
         USART2: {
-            path: rx,
-            resources: [RX, VEC_DATA, END, RB],
+            path: receive_callback,
+            resources: [RX, END, RB],
         }
     }
 }
 
-// static buf: [[u8; 8]; 2] = [[0; 8]; 2];
-
-fn init(mut p: init::Peripherals, r: init::Resources) -> init::LateResources {
-    // `init` can modify all the `resources` declared in `app!`
-
-    // p.device.RCC.iopenr.modify(|_, w| w.iopaen().set_bit());
-
-    // p.device.GPIOA.moder.modify(|_, w| unsafe{ w.mode5().bits(1) });
-
-    // // p.device.GPIOA.odr.modify(|_, w| w.od5().set_bit());
-    
-    // p.core.SYST.set_clock_source(SystClkSource::Core);
-    // p.core.SYST.set_reload(2_000_000); // 1s
-    // p.core.SYST.clear_current();
-    // p.core.SYST.enable_counter();
-    // p.core.SYST.enable_interrupt();
-
+fn init(mut p: init::Peripherals, _r: init::Resources) -> init::LateResources {
     let mut rcc = p.device.RCC.constrain();
     let mut flash = p.device.FLASH.constrain();
     let mut gpioa = p.device.GPIOA.split(&mut rcc.iop);
@@ -107,73 +88,69 @@ fn init(mut p: init::Peripherals, r: init::Resources) -> init::LateResources {
 
     let (mut tx, mut rx) = serial.split();
 
-
-    // let mut channels = p.device.DMA1.split(&mut rcc.ahb);
-    // channels.4.listen(Event::HalfTransfer);
-    // channels.4.listen(Event::TransferComplete);
-
     init::LateResources {
         RX: rx,
         LED: led,
+        TX: tx,
     }
 }
 
-fn idle(t: &mut Threshold, mut r: idle::Resources) -> ! {
-    
-    let mut hstdout = hio::hstdout().unwrap();
-
-    // let mut xs: Vec<u8, U8> = Vec::new(); // can hold up to 8 elements
-    // r.VEC_DATA.push(42).unwrap();
-
+fn idle(mut t: &mut Threshold, mut r: idle::Resources) -> ! {
+    // let mut hstdout = hio::hstdout().unwrap();
 
     loop {
-        // rtfm::wfi();
+        rtfm::wfi();
 
-        r.LED.set_high();
+        if r.END.claim(&mut t, |end, _| *end) {
+            let mut vec: Vec<u8, U20> = Vec::new();
 
-        let end = r.END.claim_mut(t, |end, _| {
-            if *end{
-                *end != *end;
+            loop{
+                let c = r.RB.claim_mut(t, |rb, _| rb.dequeue());
+
+                match c {
+                    Some(data) => 
+                        {
+                            if data != 32 {
+                                vec.push(data);
+                            }
+                        },
+                    _ => break,
+                };
             }
-            *end
-        });
 
-        if !end {
-            r.LED.set_low();
-            writeln!(hstdout,"end").unwrap();
-            let a = r.RB.claim_mut(t, |rb, _| rb.dequeue());
+            // for (i, e) in vec.iter().enumerate(){
+            //     block!(r.TX.write(*e));
+            // }
+
+            let op = vec.get(4);
+
+            let command =
+                match op {
+                    Some(78) | Some (110) => Command::Enable,
+                    Some(70) | Some (102) => Command::Disable,
+                    _ => Command::Nothing,
+                };
+            
+            match command {
+                Command::Enable => r.LED.set_high(),
+                Command::Disable => r.LED.set_low(),
+                _ => (),
+            }
+
+            r.END.claim_mut(&mut t, |end, _| *end = false);
         }
-
-        // let data = *r.DATA;
-        // let data = r.DATA.data;
-        // if data == 1 {
-        //     r.LED.set_high();
-        // }
-        // else {
-        //     r.LED.set_low();
-        // }
     }
 }
 
-fn rx(_t: &mut Threshold, mut r: USART2::Resources) {
-    // r.CB
-    //     .peek(|_buf, _half| {
-    //         bkpt();
-    //     })
-    //     .unwrap();
-    // bkpt();
+fn receive_callback(_t: &mut Threshold, mut r: USART2::Resources) {
+    let data = r.RX.read().unwrap();
 
-    let a = r.RX.read().unwrap();
-
-    if a == 0x10 {
+    if data == 10 {
         *r.END = true;
-    } else {
-        r.VEC_DATA.push(a).unwrap();
-        r.RB.enqueue(a);
-    }
-
-
-    // bkpt();
+    } 
+    
+    let mut producer = unsafe { r.RB.split().0 };
+    producer.enqueue(data);
 }
 
 
