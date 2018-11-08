9df04724c224a67d666bd03e1e2dddc59baf55d1
Cecília Carneiro <cecilia.carneiroesilva@gmail.com>
2018-11-08 09:26:23 -0200

    fsm complete - first version

--- examples/fsm.rs
+++ examples/fsm.rs
@@ -36,7 +36,7 @@ use hal::dma::DmaExt;
 use hal::dma::CircBufferLinear;
 use hal::dma::dma1;
 use hal::stm32l052;
-use hal::stm32l052::USART1 as USART1_p;
+use hal::stm32l052::USART2 as USART2_p;
 use hal::gpio::{Output, Input, PushPull, PullUp, gpioa::PA5, gpioa::PA4};
 
 enum Command {
@@ -57,7 +57,6 @@ pub enum StateType {
     Start,
     Auto,
     Manual,
-    SteadyState,
 }
 
 impl FiniteMachine {
@@ -66,32 +65,38 @@ impl FiniteMachine {
             state: StateType::Start,
         }
     }
+
+    pub fn start(&mut self) {
+        self.state = StateType::Auto;
+    }
 }
 
 app! {
     device: stm32l052,
     resources: {
         static END: bool = false;
-        static TX: Tx<USART1_p>;
-        static RI: ReleaseInterupt<USART1_p>;
+        static TX: Tx<USART2_p>;
+        static RI: ReleaseInterupt<USART2_p>;
         static LED: PA5<Output<PushPull>>;
-        static CB: CircBufferLinear<[u8; SIZE_BUF], dma1::C3>;
+        static CB: CircBufferLinear<[u8; SIZE_BUF], dma1::C5>;
         static BUTTON: PA4<Input<PullUp>>;
+        static FSM: FiniteMachine;
+        static EXTI: stm32l052::EXTI;
     },
     idle: {
-        resources: [LED, END, TX, CB, BUTTON],
+        resources: [LED, END, TX, CB, BUTTON, FSM],
     },
     tasks: {
-        USART1: {
+        USART2: {
             priority: 1,
             path: end_frame_callback,
             resources: [END, RI],
         },
-        // EXTI4_15: {
-        //     priority: 1,
-        //     path: button_callback,
-        //     resources: [BUTTON/* , LED */],
-        // },
+        EXTI4_15: {
+            priority: 1,
+            path: button_callback,
+            resources: [BUTTON, LED, FSM, EXTI],
+        },
     }
 }
 
@@ -102,23 +107,24 @@ fn init(mut p: init::Peripherals, _r: init::Resources) -> init::LateResources {
     let mut channels = p.device.DMA1.split(&mut rcc.ahb);
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    // p.device.SYSCFG_COMP.exticr2.modify(|_, w| unsafe{ w.exti4().bits(0b0000) });//PA0
+    p.device.SYSCFG_COMP.exticr2.modify(|_, w| unsafe{ w.exti4().bits(0b0000) });//PA0
 
-    // p.device.EXTI.imr.modify(|_, w| w.im4().bit(true));
-    // p.device.EXTI.ftsr.modify(|_, w| w.ft4().bit(true));
+    p.device.EXTI.imr.modify(|_, w| w.im4().bit(true));
+    p.device.EXTI.ftsr.modify(|_, w| w.ft4().bit(true));
+    p.device.EXTI.rtsr.modify(|_, w| w.rt4().bit(true));
 
     let mut led = gpioa.pa5.into_output(&mut gpioa.moder).push_pull(&mut gpioa.otyper);
-    let but = gpioa.pa4.into_input(&mut gpioa.moder).pull_up(&mut gpioa.pupdr);
+    let mut but = gpioa.pa4.into_input(&mut gpioa.moder).pull_up(&mut gpioa.pupdr);
 
-    let tx = gpioa.pa9.into_alternate(&mut gpioa.moder).af4(&mut gpioa.afrh);
-    let rx = gpioa.pa10.into_alternate(&mut gpioa.moder).af4(&mut gpioa.afrh);
+    let tx = gpioa.pa2.into_alternate(&mut gpioa.moder).af4(&mut gpioa.afrl);
+    let rx = gpioa.pa3.into_alternate(&mut gpioa.moder).af4(&mut gpioa.afrl);
 
-    let serial = Serial::usart1(
-        p.device.USART1,
+    let serial = Serial::usart2(
+        p.device.USART2,
         (tx, rx),
         9_600.bps(),
         clocks,
-        &mut rcc.apb2,
+        &mut rcc.apb1,
         Some(LF)
     );
 
@@ -127,7 +133,10 @@ fn init(mut p: init::Peripherals, _r: init::Resources) -> init::LateResources {
     let (mut tx, mut rx, mut ri) = serial.split();
 
     let buf = singleton!(: [[u8; SIZE_BUF]; 1] = [[0; SIZE_BUF]; 1]).unwrap();
-    let cb = rx.circ_buf_linear(channels.3, buf);
+    let cb = rx.circ_buf_linear(channels.5, buf);
+
+    let mut fsm = FiniteMachine::new();
+    fsm.start();
 
     init::LateResources {
         LED: led,
@@ -135,20 +144,25 @@ fn init(mut p: init::Peripherals, _r: init::Resources) -> init::LateResources {
         RI: ri,
         CB: cb,
         BUTTON: but,
+        FSM: fsm,
+        EXTI: p.device.EXTI,
     }
 }
 
 fn idle(mut t: &mut Threshold, mut r: idle::Resources) -> ! {
-    let mut finite_machine = FiniteMachine::new();
-
-    finite_machine.state = StateType::Auto;
-
     loop {
-        // rtfm::wfi();
+        rtfm::wfi();
 
         // bkpt();
 
         if r.END.claim(&mut t, |end, _| *end) {
+            let state = r.FSM.claim(&mut t, |fsm, _| 
+                match fsm.state {
+                    StateType::Auto => StateType::Auto,
+                    StateType::Manual => StateType::Manual,
+                    StateType::Start => StateType::Start,
+            });
+
             let (buf1, buf2) = r.CB.partial_peek();
 
             let mut vec: Vec<u8, U8> = Vec::new();
@@ -160,21 +174,18 @@ fn idle(mut t: &mut Threshold, mut r: idle::Resources) -> ! {
                 }
             }
 
-            // for v in &vec[..] {
-            //     block!(r.TX.write(*v)).ok();
-            // }
+            for v in &vec[..] {
+                block!(r.TX.write(*v)).ok();
+            }
 
-            // let state = 
-            //     match &vec[..] {
-            //         [b'a'/* , b'u', b't', b'o' */] => StateType::Auto,
-            //         [b'm', b'a', b'n', b'u', b'a', b'l'] => StateType::Manual,
-            //         _ => StateType::SteadyState,
-            // };
-
-            // finite_machine.state = state;
-
-            //if state == auto tentar quebrar o frame.
-            match finite_machine.state {
+            let new_state = 
+                match &vec[..] {
+                    [b'a', b'u', b't', b'o'] => StateType::Auto,
+                    [b'm', b'a', b'n', b'u', b'a', b'l'] => StateType::Manual,
+                    _ => state,
+            };
+            
+            match new_state {
                 StateType::Auto => {
                     let op = vec.get(4);
 
@@ -186,32 +197,40 @@ fn idle(mut t: &mut Threshold, mut r: idle::Resources) -> ! {
                         };
                     
                     match command {
-                        Command::Enable => r.LED.set_high(),
-                        Command::Disable => r.LED.set_low(),
+                        Command::Enable => r.LED.claim_mut(&mut t, |led, _| led.set_high()),
+                        Command::Disable => r.LED.claim_mut(&mut t, |led, _| led.set_low()),
                         _ => (),
                     }
                 },
                 _ => (),
             }
 
+            r.FSM.claim_mut(&mut t, |fsm, _| fsm.state = new_state);
         }
         
         r.END.claim_mut(&mut t, |end, _| *end = false);
     }
 }
 
-fn end_frame_callback(_t: &mut Threshold, mut r: USART1::Resources) {
+fn end_frame_callback(_t: &mut Threshold, mut r: USART2::Resources) {
     *r.END = true;
     r.RI.clear_isr_cmie();
 }
 
-// fn button_callback(_t: &mut Threshold, mut r: EXTI4_15::Resources) {
-//     if r.BUTTON.is_low() {
-//         // r.LED.set_high();
-//     } else {
-//         // r.LED.set_low();
-//     }
-// }
+fn button_callback(_t: &mut Threshold, mut r: EXTI4_15::Resources) {
+    match r.FSM.state {
+        StateType::Manual => {
+            if r.BUTTON.is_low() {
+                r.LED.set_high();
+            } else {
+                r.LED.set_low();
+            }
+        },
+        _ => {},
+    };
+
+    r.EXTI.pr.modify(|_, w| w.pif0().bit(true));
+}
 
 #[allow(deprecated)]
 #[panic_implementation]
