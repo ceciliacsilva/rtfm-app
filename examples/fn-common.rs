8957a3ec0d9055bd74e092e1e085a5d2a93c94d4
Cecília Carneiro <cecilia.carneiroesilva@gmail.com>
2019-02-15 08:17:58 -0200

    fn-common - trait objects fail;

--- examples/fn-common.rs
+++ examples/fn-common.rs
@@ -7,8 +7,10 @@ extern crate rtfm;
 extern crate narc_hal;
 extern crate embedded_hal;
 extern crate timer_wheels;
+extern crate heapless;
 
 use rtfm::app;
+use rtfm::Mutex;
 use rtfm::export::wfi;
 use core::panic::PanicInfo;
 use core::sync::atomic::{self, Ordering};
@@ -18,42 +20,62 @@ use narc_hal::rcc::RccExt;
 use narc_hal::gpio::GpioExt;
 use narc_hal::gpio::{gpioa::PA5, Output, PushPull};
 use embedded_hal::digital::OutputPin;
+use heapless::{
+    consts::*,
+    spsc::{Consumer, Producer, Queue},
+};
 
 #[app(device = narc_hal::stm32l052)]
 const APP: () = {
-    static mut LED: PA5<Output<PushPull>> = ();
+    static mut Q: Option<Queue<Event, U4>> = None;
+    static mut P: Producer<'static, Event, U4> = ();
+    static mut C: Consumer<'static, Event, U4> = ();
+    // static mut LED: PA5<Output<PushPull>> = ();
+    static mut E_LED_ON: Event = ();
 
-    #[init(spawn = [foo])]
+    #[init(spawn = [foo], resources = [Q])]
     fn init() {
         let mut rcc = device.RCC.constrain();
         let mut gpioa = device.GPIOA.split(&mut rcc.iop);
         let led = gpioa.pa5.into_output(&mut gpioa.moder).push_pull(&mut gpioa.otyper);
-        spawn.foo().unwrap();
 
-        LED = led;
+        spawn.foo();
+
+        *resources.Q = Some(Queue::new());
+        let (p, c) = resources.Q.as_mut().unwrap().split();
+
+        // construir o evento nao o setledon
+        E_LED_ON =
+            Event {
+                e: &mut
+                    SetLedOn{
+                        led: led,
+                    }
+            };
+
+        P = p;
+        C = c;
+        // LED = led;
     }
 
     #[idle]
     fn idle() -> ! {
-        wfi();
         loop {
-            unimplemented!();
+            wfi();
         }
     }
 
-    #[task(resources = [LED])]
+    #[task(resources = [P, E_LED_ON])]
     fn foo() {
-        common(resources.LED);
-    }
+        // *resources.E_LED_ON = Some(
+        // );
+        // spawn.bar().unwrap();
+        // resources.EVENT.run();
 
-    #[task(spawn = [baz])]
-    fn bar() {
-        spawn.baz();
-    }
-
-    #[task(resources = [LED])]
-    fn baz() {
-        common(resources.LED);
+        // resources.P.enqueue(
+        //     Event {
+        //         e: resources.E_LED_ON,
+        //     });
     }
 
     extern "C" {
@@ -61,6 +83,62 @@ const APP: () = {
     }
 
 };
+
+
+pub trait ValidEvent {
+    fn run(&mut self);
+}
+
+pub struct Event
+{
+    e: &'static mut ValidEvent,
+}
+
+unsafe impl Send for Event{}
+
+pub struct SetLedOn
+// where
+    // T: Mutex<T = PA5<Output<PushPull>>>,
+{
+    // led: &'a mut PA5<Output<PushPull>>,
+    led: PA5<Output<PushPull>>,
+    // led: bool,
+}
+
+impl ValidEvent for SetLedOn
+// where
+    // T: Mutex<T = PA5<Output<PushPull>>>
+{
+    fn run(&mut self) {
+        use rtfm::Mutex;
+
+        // self.led.lock(
+            // |led| {
+        // self.led.set_high(); 
+        // });
+    }
+}
+
+pub struct EventDisableLed
+// where
+// T: Mutex<T = PA5<Output<PushPull>>>,
+{
+    led: PA5<Output<PushPull>>,
+}
+
+impl ValidEvent for EventDisableLed
+// where
+// T: Mutex<T = PA5<Output<PushPull>>>
+{
+    fn run(&mut self) {
+        use rtfm::Mutex;
+
+        // self.led.lock(
+        // |led| {
+        self.led.set_low();
+        // });
+    }
+}
 
 fn common(mut shared_led: impl rtfm::Mutex<T = PA5<Output<PushPull>>>){
     shared_led.lock(|led| {
